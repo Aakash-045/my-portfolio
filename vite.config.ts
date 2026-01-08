51d563fd00c000c128082cd10f1b1d1471a1e25c
unknown <aakashmuthukrishnan18@gmail.com>
2026-01-08 17:45:45 +0530

    INITIAL COMMIT

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", // <-- use relative paths locally
+  base: "/my-portfolio/", // Unga repo name
 });
